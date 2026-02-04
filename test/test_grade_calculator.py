8971603bde119808e94813dc73fdb033db251cbd
VidhyaWalke <mscds.2420@unigoa.ac.in>
2026-02-04 10:00:28 +0530

    test cases correctly given

--- test/test_grade_calculator.py
+++ test/test_grade_calculator.py
@@ -8,9 +8,4 @@ def test_Student_Grade_Calculator():
     assert Student_Grade_Calculator(70, 80, 90, 100, 50) == "A"
 
 
-# def test_Student_Grade_Calculator_fail():
-
-#     assert Student_Grade_Calculator(70, 80, 90, 100, 50) == "B"
-
-
 # print(test_Student_Grade_Calculator())
